3deb5ea00db202972fa4be7285f369a1d859dc97
Seu nome <Seu email de cadastro do Github>
2023-11-04 19:01:32 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiroComponenteComponent } from './components/primeiro-componente/primeiro-componente.component';
+import { ButtonComponent } from './button/button.component';
+import { MenuComponent } from './menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {FormsModule} from '@angular/forms';
+import { InputComponent } from './input/input.component';
 @NgModule({
+   
   declarations: [
     AppComponent,
-    PrimeiroComponenteComponent
+    PrimeiroComponenteComponent,
+    ButtonComponent,
+    InputComponent,
+
+          
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MenuComponent,
+    FormsModule, 
+  
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { };
+
